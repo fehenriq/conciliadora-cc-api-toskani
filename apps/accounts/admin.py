1fadecae5e32bbef67805ce9d6a7b78421b2d284
Felipe <lipe.h.r@gmail.com>
2024-10-16 16:05:40 -0300

    :sparkles: feat: admin config

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, Installment, OmieAccount
+
+
+class InstallmentInline(admin.TabularInline):
+    model = Installment
+    extra = 1
+
+
+@admin.register(OmieAccount)
+class OmieAccountAdmin(admin.ModelAdmin):
+    list_display = ("account_number", "description", "omie_id")
+    search_fields = ("account_number", "description", "omie_id")
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("omie_account", "acquirer", "settle", "days_to_receive")
+    list_filter = ("acquirer", "settle")
+    search_fields = ("omie_account__account_number", "acquirer")
+    inlines = [InstallmentInline]
+
+
+@admin.register(Installment)
+class InstallmentAdmin(admin.ModelAdmin):
+    list_display = ("account", "installment_number", "fee")
+    list_filter = ("installment_number",)
+    search_fields = ("account__omie_account__account_number", "installment_number")
